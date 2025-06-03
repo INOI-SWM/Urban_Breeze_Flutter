940d7dd80ad345d1cf1efa932df08bb47cb04049
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-03 15:40:43 +0900

    chore: Icon 크기 상수 지정

---
+++ lib/design_system/Icon/app_icon_constants.dart
@@ -0,0 +1,9 @@
+class AppIconConstants {
+  const AppIconConstants._();
+
+  static const int xlarge = 16;
+  static const int large = 32;
+  static const int medium = 28;
+  static const int small = 24;
+  static const int xsmall = 20;
+}
