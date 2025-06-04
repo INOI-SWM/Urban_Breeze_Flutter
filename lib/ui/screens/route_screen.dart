48111477c136281fc94afb9b4c9fef01c2a5aa3e
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2025-06-04 19:59:49 +0900

    feat: 내비게이션 내 화면 위젯 추가

---
+++ lib/ui/screens/route_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RouteScreen extends StatelessWidget {
+  const RouteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('경로 생성'));
+  }
+}
