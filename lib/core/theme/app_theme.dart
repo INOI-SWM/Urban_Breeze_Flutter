0198a4738ffdf3926a6f28af6a158e964d9657ae
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2025-06-05 01:22:32 +0900

    feat: AppTheme에 NavigationBarThemeData 추가

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:ridingmate/core/theme/semantic_colors.dart';
+import 'package:ridingmate/design_system/typography/app_text_style.dart'; // For AppTextStyles
 
 class AppTheme {
-  static final ThemeData lightTheme = ThemeData(brightness: Brightness.light);
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    navigationBarTheme: NavigationBarThemeData(
+      labelTextStyle: WidgetStateProperty.resolveWith<TextStyle>((
+        Set<WidgetState> states,
+      ) {
+        if (states.contains(WidgetState.selected)) {
+          return AppTextStyles.caption2.medium.copyWith(
+            color: const LightSemanticColors().primaryNormal,
+          );
+        }
+        return AppTextStyles.caption2.medium.copyWith(
+          color: const LightSemanticColors().labelNeutral,
+        );
+      }),
+      iconTheme: WidgetStateProperty.resolveWith<IconThemeData>((
+        Set<WidgetState> states,
+      ) {
+        if (states.contains(WidgetState.selected)) {
+          return IconThemeData(
+            color: const LightSemanticColors().primaryNormal,
+          );
+        }
+        return IconThemeData(color: const LightSemanticColors().labelNeutral);
+      }),
+    ),
+  );
 
-  static final ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+  static final ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    navigationBarTheme: NavigationBarThemeData(
+      labelTextStyle: WidgetStateProperty.resolveWith<TextStyle>((
+        Set<WidgetState> states,
+      ) {
+        if (states.contains(WidgetState.selected)) {
+          return AppTextStyles.caption2.medium.copyWith(
+            color: const DarkSemanticColors().primaryNormal,
+          );
+        }
+        return AppTextStyles.caption2.medium.copyWith(
+          color: const DarkSemanticColors().labelNeutral,
+        );
+      }),
+      iconTheme: WidgetStateProperty.resolveWith<IconThemeData>((
+        Set<WidgetState> states,
+      ) {
+        if (states.contains(WidgetState.selected)) {
+          return IconThemeData(color: const DarkSemanticColors().primaryNormal);
+        }
+        return IconThemeData(color: const DarkSemanticColors().labelNeutral);
+      }),
+    ),
+  );
 }
 
 class SemanticTheme extends InheritedWidget {
