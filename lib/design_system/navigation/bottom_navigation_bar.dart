f1f2a75d482c0c7cf51fa02887031e68b0f38027
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2025-06-04 19:38:01 +0900

    feat: 하단 내비게이션 바 생성

---
+++ lib/design_system/navigation/bottom_navigation_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AppNavigationBar extends StatelessWidget {
+  const AppNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onDestinationSelected,
+  });
+  final int currentIndex;
+  final ValueChanged<int> onDestinationSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: currentIndex,
+      onDestinationSelected: onDestinationSelected,
+      destinations: const <Widget>[
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined),
+          selectedIcon: Icon(Icons.home),
+          label: '홈',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.route_outlined),
+          selectedIcon: Icon(Icons.route),
+          label: '경로 생성',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.directions_bike_outlined),
+          selectedIcon: Icon(Icons.directions_bike),
+          label: '라이딩',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.bar_chart_outlined),
+          selectedIcon: Icon(Icons.bar_chart),
+          label: '운동 기록',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person_outline),
+          selectedIcon: Icon(Icons.person),
+          label: 'MY',
+        ),
+      ],
+    );
+  }
+}
