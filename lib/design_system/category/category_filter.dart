4542dc16a5278a07fbeb2435456cce3c9f076ef0
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-05 17:27:25 +0900

    feat: 카테고리 필터 크기 조정 기능 추가

--- lib/design_system/category/category_filter.dart
+++ lib/design_system/category/category_filter.dart
@@ -2,19 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:ridingmate/core/theme/extensions.dart';
 import 'package:ridingmate/design_system/chip/chip_action.dart';
 
+enum CategoryFilterSize { small, medium, large, xlarge }
+
 class CategoryFilter extends StatelessWidget {
   const CategoryFilter({
     super.key,
     required this.categories,
     required this.selectedCategories,
     required this.onCategorySelected,
-    this.spacing = 10,
+    this.size = CategoryFilterSize.medium,
   });
 
   final List<String> categories;
   final Set<String> selectedCategories;
   final void Function(String category) onCategorySelected;
-  final double spacing;
+  final CategoryFilterSize size;
+
+  double get _spacing {
+    switch (size) {
+      case CategoryFilterSize.small:
+        return 4;
+      case CategoryFilterSize.medium:
+        return 6;
+      case CategoryFilterSize.large:
+        return 8;
+      case CategoryFilterSize.xlarge:
+        return 10;
+    }
+  }
+
+  ChipActionSize get _chipSize {
+    switch (size) {
+      case CategoryFilterSize.small:
+        return ChipActionSize.xsmall;
+      case CategoryFilterSize.medium:
+        return ChipActionSize.small;
+      case CategoryFilterSize.large:
+        return ChipActionSize.medium;
+      case CategoryFilterSize.xlarge:
+        return ChipActionSize.large;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +54,11 @@ class CategoryFilter extends StatelessWidget {
               final bool isSelected = selectedCategories.contains(category);
               return Padding(
                 padding: EdgeInsets.only(
-                  right: category == categories.last ? 0 : spacing,
+                  right: category == categories.last ? 0 : _spacing,
                 ),
                 child: ChipAction(
                   text: category,
-                  size: ChipActionSize.medium,
+                  size: _chipSize,
                   type: ChipActionType.outlined,
                   textColor:
                       isSelected
