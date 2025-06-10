e5f7ef51c1211ee1d3aac59a12e9a35e2e08710d
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-10 23:36:37 +0900

    feat: 지도에 찍을 핀 이미지 생성, 지도 시작 시 현위치에서 시작, 현위치에 핀찍기

--- lib/ui/screens/riding_screen.dart
+++ lib/ui/screens/riding_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 class RidingScreen extends StatefulWidget {
@@ -11,8 +12,40 @@ class RidingScreen extends StatefulWidget {
 }
 
 class _RidingScreenState extends State<RidingScreen> {
-  final LatLng initialCenter = const LatLng(37.5665, 126.9780);
-  final double initialZoom = 13.0;
+  final LatLng initialCenter = const LatLng(37.5665, 126.9780); //서울시청
+  final double initialZoom = 16.0;
+  LatLng? _currentPosition;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      final LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        final LocationPermission requestPermission =
+            await Geolocator.requestPermission();
+        if (requestPermission == LocationPermission.denied) {
+          return;
+        }
+      }
+
+      final Position position = await Geolocator.getCurrentPosition();
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final String baseUrl =
@@ -20,9 +53,13 @@ class _RidingScreenState extends State<RidingScreen> {
     final String apiKey = dotenv.env['THUNDERFOREST_API_KEY'] ?? 'fallback_key';
     final String fullUrlTemplate = '$baseUrl?apikey=$apiKey';
 
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
     return FlutterMap(
       options: MapOptions(
-        initialCenter: initialCenter,
+        initialCenter: _currentPosition ?? initialCenter,
         initialZoom: initialZoom,
         interactionOptions: const InteractionOptions(
           flags: InteractiveFlag.all,
@@ -33,6 +70,17 @@ class _RidingScreenState extends State<RidingScreen> {
           urlTemplate: fullUrlTemplate,
           userAgentPackageName: 'com.example.ridingmate',
         ),
+        if (_currentPosition != null)
+          MarkerLayer(
+            markers: <Marker>[
+              Marker(
+                point: _currentPosition!,
+                width: 32,
+                height: 32,
+                child: Image.asset('assets/icons/png/current_location_pin.png'),
+              ),
+            ],
+          ),
       ],
     );
   }
