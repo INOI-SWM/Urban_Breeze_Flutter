d1d616e284018f56c385969e56dcd2719e0b52c2
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-14 20:37:58 +0900

    feat: api응답 model 생성, 기존 사용하던 모델 명칭 변경

--- lib/models/route.dart
+++ lib/models/route.dart
@@ -1,7 +1,7 @@
 import 'package:latlong2/latlong.dart';
 
-class RouteResult {
-  RouteResult({
+class Route {
+  Route({
     required this.points,
     required this.distance,
     required this.duration,
@@ -9,10 +9,11 @@ class RouteResult {
     required this.descent,
     required this.elevationGain,
   });
+
   final List<LatLng> points;
-  final double distance; // 단위: 미터
-  final double duration; // 단위: 초
+  final double distance; // 미터
+  final double duration; // 초
   final double ascent; // ORS 원시 총 상승고도(미터)
   final double descent; // 총 하강고도(미터)
-  final double elevationGain; //의미있는 상승고도(미터)
+  final double elevationGain; // Strava 스타일 의미있는 상승고도(미터)
 }
