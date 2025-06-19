baa537c56b87b7d1f9a0b91f8b036e835e1f3631
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-19 16:01:13 +0900

    feat: 소셜 로그인 아이콘추가 및 버튼생성

--- lib/features/profile/presentation/screens/my_screen.dart
+++ lib/features/profile/presentation/screens/my_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:ridingmate/features/profile/presentation/widgets/login_button.dart';
 
 class MyScreen extends StatelessWidget {
   const MyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('MY'));
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              LoginButton(
+                text: 'Google로 계속하기',
+                iconPath: 'assets/icons/svg/google_logo.svg',
+                onPressed: () {
+                  // TODO: Google 로그인 처리
+                },
+              ),
+              const SizedBox(height: 12),
+              LoginButton(
+                text: 'Apple로 계속하기',
+                iconPath: 'assets/icons/svg/apple_logo.svg',
+                onPressed: () {
+                  // TODO: Apple 로그인 처리
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
