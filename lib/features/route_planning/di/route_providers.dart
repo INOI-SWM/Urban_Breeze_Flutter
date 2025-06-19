5d83f4e8df53c256aa0bed2895bc7da0d97a7ab4
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-20 01:48:45 +0900

    feat: route_planning di provider생성

---
+++ lib/features/route_planning/di/route_providers.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:ridingmate/features/route_planning/data/datasources/route_remote_datasource.dart';
+import 'package:ridingmate/features/route_planning/data/repositories/route_repository_impl.dart';
+import 'package:ridingmate/features/route_planning/domain/repositories/route_repository.dart';
+
+final Provider<http.Client> httpClientProvider = Provider<http.Client>((
+  Ref<http.Client> ref,
+) {
+  return http.Client();
+});
+
+final Provider<RouteRemoteDataSource> routeRemoteDataSourceProvider =
+    Provider<RouteRemoteDataSource>((Ref<RouteRemoteDataSource> ref) {
+      final http.Client client = ref.watch(httpClientProvider);
+      return RouteRemoteDataSourceImpl(client: client);
+    });
+
+final Provider<RouteRepository> routeRepositoryProvider =
+    Provider<RouteRepository>((Ref<RouteRepository> ref) {
+      final RouteRemoteDataSource remoteDataSource = ref.watch(
+        routeRemoteDataSourceProvider,
+      );
+      return RouteRepositoryImpl(remoteDataSource: remoteDataSource);
+    });
