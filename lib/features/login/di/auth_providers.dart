8ebde8482572e76d9a62e6eda109a9e16543408f
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-22 23:34:23 +0900

    feat: di설정

--- lib/features/login/di/auth_providers.dart
+++ lib/features/login/di/auth_providers.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ridingmate/features/login/application/use_cases/sign_in_with_apple_use_case.dart';
 import 'package:ridingmate/features/login/application/use_cases/sign_in_with_google_use_case.dart';
+import 'package:ridingmate/features/login/application/use_cases/sign_in_with_kakao_use_case.dart';
 import 'package:ridingmate/features/login/data/datasources/apple_auth_datasource.dart';
 import 'package:ridingmate/features/login/data/datasources/google_auth_datasource.dart';
+import 'package:ridingmate/features/login/data/datasources/kakao_auth_datasource.dart';
 import 'package:ridingmate/features/login/data/repositories/apple_auth_repository_impl.dart';
 import 'package:ridingmate/features/login/data/repositories/google_auth_repository_impl.dart';
+import 'package:ridingmate/features/login/data/repositories/kakao_auth_repository_impl.dart';
 import 'package:ridingmate/features/login/domain/repositories/apple_auth_repository.dart';
 import 'package:ridingmate/features/login/domain/repositories/google_auth_repository.dart';
+import 'package:ridingmate/features/login/domain/repositories/kakao_auth_repository.dart';
 
 //  DataSource Providers
 final Provider<GoogleAuthDataSource> googleAuthDataSourceProvider =
@@ -17,6 +21,11 @@ final Provider<GoogleAuthDataSource> googleAuthDataSourceProvider =
 final Provider<AppleAuthDataSource> appleAuthDataSourceProvider =
     Provider<AppleAuthDataSource>((Ref<AppleAuthDataSource> ref) {
       return AppleAuthDataSourceImpl();
+    });
+
+final Provider<KakaoAuthDataSource> kakaoAuthDataSourceProvider =
+    Provider<KakaoAuthDataSource>((Ref<KakaoAuthDataSource> ref) {
+      return KakaoAuthDataSourceImpl();
     });
 
 //  Repository Providers
@@ -38,6 +47,14 @@ final Provider<AppleAuthRepository> appleAuthRepositoryProvider =
       return AppleAuthRepositoryImpl(appleAuthDataSource: appleAuthDataSource);
     });
 
+final Provider<KakaoAuthRepository> kakaoAuthRepositoryProvider =
+    Provider<KakaoAuthRepository>((Ref<KakaoAuthRepository> ref) {
+      final KakaoAuthDataSource kakaoAuthDataSource = ref.watch(
+        kakaoAuthDataSourceProvider,
+      );
+      return KakaoAuthRepositoryImpl(kakaoAuthDataSource: kakaoAuthDataSource);
+    });
+
 //  Use Case Providers
 final Provider<SignInWithGoogleUseCase> signInWithGoogleUseCaseProvider =
     Provider<SignInWithGoogleUseCase>((Ref<SignInWithGoogleUseCase> ref) {
@@ -53,4 +70,12 @@ final Provider<SignInWithAppleUseCase> signInWithAppleUseCaseProvider =
         appleAuthRepositoryProvider,
       );
       return SignInWithAppleUseCase(repository: appleAuthRepository);
+    });
+
+final Provider<SignInWithKakaoUseCase> signInWithKakaoUseCaseProvider =
+    Provider<SignInWithKakaoUseCase>((Ref<SignInWithKakaoUseCase> ref) {
+      final KakaoAuthRepository kakaoAuthRepository = ref.watch(
+        kakaoAuthRepositoryProvider,
+      );
+      return SignInWithKakaoUseCase(repository: kakaoAuthRepository);
     });
