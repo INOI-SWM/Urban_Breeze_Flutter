99a4c597c295ba44dce223618cb64931c7c9a645
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-23 14:42:24 +0900

    feat: 서버로 보낼 bounding box 계산

--- lib/features/route_planning/application/use_cases/save_route_use_case.dart
+++ lib/features/route_planning/application/use_cases/save_route_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ridingmate/features/route_planning/domain/entities/route_data.dart';
+import 'package:ridingmate/features/route_planning/domain/services/bbox_service.dart';
 import 'package:ridingmate/features/route_planning/domain/services/polyline_convert_service.dart';
 
 class SaveRouteUseCase {
@@ -9,9 +10,28 @@ class SaveRouteUseCase {
         routeSegments,
       );
 
-      // todo: 인코딩된 Polyline을 서버에 전송하여 저장 (썸네일은 서버에서 생성)
+      final List<List<double>?> allBboxes =
+          routeSegments.map((RouteData segment) => segment.bbox).toList();
+
+      final List<double>? mergedBbox = BboxService.mergeBboxes(allBboxes);
+
+      List<double>? thumbnailBbox;
+      if (mergedBbox != null) {
+        thumbnailBbox = BboxService.expandBbox(
+          mergedBbox,
+          paddingRatio: 0.2, // 20% 패딩 추가
+        );
+      }
+
+      // todo: 인코딩된 Polyline과 바운딩 박스를 서버에 전송하여 저장 (썸네일은 서버에서 생성)
       debugPrint('저장할 경로 제목: $title');
       debugPrint('인코딩된 Polyline: $encodedPolyline');
+      if (thumbnailBbox != null) {
+        debugPrint('썸네일 바운딩 박스: ${BboxService.bboxToString(thumbnailBbox)}');
+        debugPrint(
+          'Geoapify 형식: rect:${thumbnailBbox[0]},${thumbnailBbox[1]},${thumbnailBbox[2]},${thumbnailBbox[3]}',
+        );
+      }
     } catch (e) {
       throw Exception(e);
     }
