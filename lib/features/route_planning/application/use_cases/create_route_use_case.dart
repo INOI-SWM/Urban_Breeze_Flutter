f1b8fad87c0b8c4ebb5c4d8e6680388237ec2c07
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-24 14:44:45 +0900

    feat: 서버오류, 파싱오류 등 로직오류 usecase에서 잡기

--- lib/features/route_planning/application/use_cases/create_route_use_case.dart
+++ lib/features/route_planning/application/use_cases/create_route_use_case.dart
@@ -1,4 +1,5 @@
 import 'package:latlong2/latlong.dart';
+import 'package:ridingmate/features/route_planning/data/exceptions/route_exceptions.dart';
 import 'package:ridingmate/features/route_planning/domain/entities/route_data.dart';
 import 'package:ridingmate/features/route_planning/domain/repositories/route_repository.dart';
 
@@ -9,10 +10,16 @@ class CreateRouteUseCase {
   final RouteRepository _routeRepository;
 
   Future<RouteData?> execute(LatLng startPoint, LatLng endPoint) async {
-    final RouteData? result = await _routeRepository.getRoute(
-      startPoint,
-      endPoint,
-    );
-    return result;
+    try {
+      final RouteData? result = await _routeRepository.getRoute(
+        startPoint,
+        endPoint,
+      );
+      return result;
+    } on RouteNetworkException {
+      rethrow;
+    } catch (e) {
+      return null;
+    }
   }
 }
