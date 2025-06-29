ea1dbc00771e7088ad81ab58bc4f4dff3d194608
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-29 21:25:31 +0900

    feat: appbar 생성

--- lib/features/profile/presentation/screens/profile_edit_screen.dart
+++ lib/features/profile/presentation/screens/profile_edit_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ridingmate/features/auth/domain/entities/user.dart';
+import 'package:ridingmate/shared/design_system/widgets/app_bar/custom_app_bar.dart';
 
 class ProfileEditScreen extends ConsumerWidget {
   const ProfileEditScreen({super.key, required this.user});
@@ -10,7 +11,17 @@ class ProfileEditScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(title: const Text('프로필 수정')),
+      appBar: CustomAppBar(
+        title: '프로필 수정',
+        leading: GestureDetector(
+          child: const SizedBox(
+            width: 24,
+            height: 24,
+            child: Icon(Icons.arrow_back_ios_new, size: 24),
+          ),
+        ),
+      ),
+
       body: const Center(child: Text('프로필 수정 화면')),
     );
   }
