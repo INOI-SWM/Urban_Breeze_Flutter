7d1f09aeaa284171571d95aef1117b35e3ab078d
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-06-29 21:47:46 +0900

    feat: profile_edit_item widget생성, 화면 조립

--- lib/features/profile/presentation/screens/profile_edit_screen.dart
+++ lib/features/profile/presentation/screens/profile_edit_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ridingmate/core/extensions/theme_extensions.dart';
 import 'package:ridingmate/features/auth/domain/entities/user.dart';
+import 'package:ridingmate/features/profile/presentation/widgets/profile_edit_item.dart';
+import 'package:ridingmate/shared/design_system/tokens/semantic_colors.dart';
 import 'package:ridingmate/shared/design_system/widgets/app_bar/custom_app_bar.dart';
 
-class ProfileEditScreen extends ConsumerWidget {
+class ProfileEditScreen extends StatelessWidget {
   const ProfileEditScreen({super.key, required this.user});
 
   final User user;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
+    final SemanticColors colors = context.semanticColor;
+
     return Scaffold(
+      backgroundColor: colors.backgroundNormalNormal,
       appBar: CustomAppBar(
         title: '프로필 수정',
         leading: GestureDetector(
@@ -19,10 +24,50 @@ class ProfileEditScreen extends ConsumerWidget {
             height: 24,
             child: Icon(Icons.arrow_back_ios_new, size: 24),
           ),
+          onTap: () => Navigator.of(context).pop(),
         ),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 36),
 
-      body: const Center(child: Text('프로필 수정 화면')),
+              ProfileEditItem(
+                title: '닉네임',
+                currentValue: user.displayName ?? '설정되지 않음',
+                onPressed: () => <dynamic, dynamic>{},
+              ),
+
+              const SizedBox(height: 36),
+
+              ProfileEditItem(
+                title: '한 줄 소개',
+                currentValue: '자신을 소개해주세요',
+                onPressed: () => <dynamic, dynamic>{},
+              ),
+
+              const SizedBox(height: 36),
+
+              ProfileEditItem(
+                title: '성별',
+                currentValue: '한줄소개, 두줄소개, 세줄소개',
+                onPressed: () => <dynamic, dynamic>{},
+              ),
+
+              const SizedBox(height: 36),
+
+              ProfileEditItem(
+                title: '출생년도',
+                currentValue: '1999',
+                onPressed: () => <dynamic, dynamic>{},
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
