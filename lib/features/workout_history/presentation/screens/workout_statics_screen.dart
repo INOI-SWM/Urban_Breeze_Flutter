f283fe255276c6e9385c11a80e0bdeab167ddd3d
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-07-24 19:37:32 +0900

    feat: 통계레이아웃 생성

--- lib/features/workout_history/presentation/screens/workout_statics_screen.dart
+++ lib/features/workout_history/presentation/screens/workout_statics_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ridingmate/core/extensions/theme_extensions.dart';
+import 'package:ridingmate/shared/design_system/tokens/semantic_colors.dart';
+import 'package:ridingmate/shared/design_system/tokens/typography/app_text_style.dart';
+import 'package:ridingmate/shared/design_system/widgets/info/info_item.dart';
 import 'package:ridingmate/shared/design_system/widgets/segmented_control/segmented_control.dart';
 
-enum StatisticType {
+enum StatisticPeriodType {
   week,
   month,
   year,
@@ -9,14 +13,31 @@ enum StatisticType {
 
   String get label {
     switch (this) {
-      case StatisticType.week:
+      case StatisticPeriodType.week:
         return '주';
-      case StatisticType.month:
+      case StatisticPeriodType.month:
         return '월';
-      case StatisticType.year:
+      case StatisticPeriodType.year:
         return '년';
-      case StatisticType.all:
+      case StatisticPeriodType.all:
         return '전체';
+    }
+  }
+}
+
+enum StaticDataType {
+  distance,
+  elevation,
+  duration;
+
+  String get label {
+    switch (this) {
+      case StaticDataType.distance:
+        return '거리';
+      case StaticDataType.elevation:
+        return '상승 고도';
+      case StaticDataType.duration:
+        return '시간';
     }
   }
 }
@@ -29,38 +50,96 @@ class WorkoutStaticsScreen extends StatefulWidget {
 }
 
 class _WorkoutStaticsScreenState extends State<WorkoutStaticsScreen> {
-  StatisticType _selectedType = StatisticType.week;
+  StatisticPeriodType _selectedPeriodType = StatisticPeriodType.week;
+  StaticDataType _selectedDataType = StaticDataType.distance;
 
-  static const List<StatisticType> _tabs = <StatisticType>[
-    StatisticType.week,
-    StatisticType.month,
-    StatisticType.year,
-    StatisticType.all,
+  static const List<StatisticPeriodType> _periodTabs = <StatisticPeriodType>[
+    StatisticPeriodType.week,
+    StatisticPeriodType.month,
+    StatisticPeriodType.year,
+    StatisticPeriodType.all,
+  ];
+
+  static const List<StaticDataType> _dataTypeTabs = <StaticDataType>[
+    StaticDataType.distance,
+    StaticDataType.elevation,
+    StaticDataType.duration,
   ];
 
   @override
   Widget build(BuildContext context) {
+    final SemanticColors colors = context.semanticColor;
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        SegmentedControl<StatisticType>(
-          tabs: _tabs,
-          selectedTab: _selectedType,
-          onTabSelected: (StatisticType type) {
+        SegmentedControl<StatisticPeriodType>(
+          tabs: _periodTabs,
+          selectedTab: _selectedPeriodType,
+          onTabSelected: (StatisticPeriodType type) {
             setState(() {
-              _selectedType = type;
+              _selectedPeriodType = type;
             });
           },
-          labelExtractor: (StatisticType type) => type.label,
+          labelExtractor: (StatisticPeriodType type) => type.label,
         ),
 
-        const SizedBox(height: 32),
-
-        Expanded(child: _buildStatisticContent()),
+        const SizedBox(height: 20),
+        //TODO : 클릭 시 월 변경 옵션 띄우기
+        Text(
+          '25년 7월',
+          style: AppTextStyles.title3.bold.copyWith(color: colors.labelStrong),
+        ),
+        const SizedBox(height: 20),
+        SegmentedControl<StaticDataType>(
+          tabs: _dataTypeTabs,
+          selectedTab: _selectedDataType,
+          onTabSelected: (StaticDataType type) {
+            setState(() {
+              _selectedDataType = type;
+            });
+          },
+          labelExtractor: (StaticDataType type) => type.label,
+        ),
+        const SizedBox(height: 20),
+        Text(
+          '주간 라이딩 거리',
+          style: AppTextStyles.label1.normalBold.copyWith(
+            color: colors.labelAlternative,
+          ),
+        ),
+        Text(
+          '3.14 km',
+          style: AppTextStyles.display1.bold.copyWith(
+            color: colors.labelStrong,
+          ),
+        ),
+        const SizedBox(height: 20),
+        const Row(
+          children: <Widget>[
+            Expanded(
+              child: InfoItem(
+                label: '라이딩',
+                value: '1',
+                alignment: CrossAxisAlignment.start,
+              ),
+            ),
+            Expanded(
+              child: InfoItem(
+                label: '운동 시간',
+                value: '1:13:13',
+                alignment: CrossAxisAlignment.start,
+              ),
+            ),
+            Expanded(
+              child: InfoItem(
+                label: '상승 고도',
+                value: '100m',
+                alignment: CrossAxisAlignment.start,
+              ),
+            ),
+          ],
+        ),
       ],
     );
-  }
-
-  Widget _buildStatisticContent() {
-    return Container();
   }
 }
