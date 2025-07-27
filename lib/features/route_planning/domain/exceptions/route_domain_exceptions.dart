308817f4acfbf6686decc5b6ec7da17aafdef81c
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-07-27 23:19:06 +0900

    refactor: 경로생성 예외 부분 리팩토링

--- lib/features/route_planning/domain/exceptions/route_domain_exceptions.dart
+++ lib/features/route_planning/domain/exceptions/route_domain_exceptions.dart
@@ -1,53 +1,11 @@
-abstract class RouteDomainException implements Exception {
-  const RouteDomainException(this.message);
-  final String message;
+import 'package:ridingmate/shared/domain/exceptions/base_domain_exception.dart';
 
-  @override
-  String toString() => 'RouteDomainException: $message';
+/// 경로의 Bounding Box가 유효하지 않을 때
+class InvalidBboxException extends BaseDomainException {
+  const InvalidBboxException(super.message, [super.code]);
 }
 
-/// 네트워크 문제
-class RouteNetworkException extends RouteDomainException {
-  const RouteNetworkException(super.message);
-
-  @override
-  String toString() => 'RouteNetworkException: $message';
-}
-
-/// 서버 문제
-class RouteServerException extends RouteDomainException {
-  const RouteServerException(super.message);
-
-  @override
-  String toString() => 'RouteServerException: $message';
-}
-
-/// 데이터 파싱 문제
-class RouteParsingException extends RouteDomainException {
-  const RouteParsingException(super.message);
-
-  @override
-  String toString() => 'RouteParsingException: $message';
-}
-
-/// 경로 유효성 검증 문제
-class RouteValidationException extends RouteDomainException {
-  const RouteValidationException(super.message);
-
-  @override
-  String toString() => 'RouteValidationException: $message';
-}
-
-class InvalidBboxException extends RouteDomainException {
-  const InvalidBboxException(super.message);
-
-  @override
-  String toString() => 'InvalidBboxException: $message';
-}
-
-class RouteSaveException extends RouteDomainException {
-  const RouteSaveException(super.message);
-
-  @override
-  String toString() => 'RouteSaveException: $message';
+/// 경로 저장 실패 시
+class RouteSaveException extends BaseDomainException {
+  const RouteSaveException(super.message, [super.code]);
 }
