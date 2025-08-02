8a22d85b2aad3a32f20a694aeff1adf7e4d09e37
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-02 17:04:00 +0900

    feat: appbar 추가

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -4,9 +4,20 @@ import 'package:ridingmate/features/auth/di/auth_providers.dart';
 import 'package:ridingmate/features/auth/domain/entities/user.dart';
 import 'package:ridingmate/features/profile/presentation/screens/login_required_screen.dart';
 import 'package:ridingmate/features/profile/presentation/screens/profile_screen.dart';
+import 'package:ridingmate/navigation/page_with_app_bar.dart';
+import 'package:ridingmate/shared/design_system/widgets/app_bar/custom_app_bar.dart';
 
-class ProfilePage extends ConsumerWidget {
+class ProfilePage extends ConsumerWidget implements PageWithAppBar {
   const ProfilePage({super.key});
+  @override
+  PreferredSizeWidget getAppBar(BuildContext context) {
+    return CustomAppBar(
+      title: 'MY',
+      actions: <Widget>[
+        IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
