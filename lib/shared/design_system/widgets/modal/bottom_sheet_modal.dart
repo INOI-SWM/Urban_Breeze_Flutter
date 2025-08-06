5a75be28801d8bdb774d11919d3fbe62d8c4cde8
Eunhak Lee <eunhak0401@naver.com>
2025-08-06 14:56:04 +0900

    design: 바텀시트 모달 고정 높이 제거

--- lib/shared/design_system/widgets/modal/bottom_sheet_modal.dart
+++ lib/shared/design_system/widgets/modal/bottom_sheet_modal.dart
@@ -52,7 +52,6 @@ class BottomSheetModal extends StatelessWidget {
                       : null,
               centerTitle: true,
               titleTextSize: AppBarTitleSize.medium,
-              height: 64,
             ),
             // 내용 영역
             Flexible(child: content),
