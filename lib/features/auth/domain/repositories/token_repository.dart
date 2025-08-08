1a54463ec1d434a7532b8765a477693d3c30cd6d
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-08 16:43:05 +0900

    feat: token repository 및 impl 생성

---
+++ lib/features/auth/domain/repositories/token_repository.dart
@@ -0,0 +1,9 @@
+import 'package:ridingmate/features/auth/domain/entities/auth_tokens.dart';
+
+abstract class TokenRepository {
+  Future<void> saveTokens(AuthTokens tokens);
+  Future<AuthTokens?> loadTokens();
+  Future<void> clearTokens();
+  Future<String?> getAccessToken();
+  Future<String?> getRefreshToken();
+}
