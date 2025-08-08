c9dc79cf3a6ed3f174cdc88ab309d162e2046467
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-08 13:39:49 +0900

    feat: di 생성

--- lib/features/auth/data/repositories/ridingmate_auth_repository_impl.dart
+++ lib/features/auth/data/repositories/ridingmate_auth_repository_impl.dart
@@ -1,11 +1,12 @@
-import 'package:ridingmate/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:ridingmate/features/auth/data/datasources/ridingmate_auth_remote_datasource.dart';
 import 'package:ridingmate/features/auth/domain/repositories/ridingmate_auth_repository.dart';
 
 class RidingMateAuthRepositoryImpl implements RidingMateAuthRepository {
-  RidingMateAuthRepositoryImpl({required AuthRemoteDataSource remoteDataSource})
-    : _remoteDataSource = remoteDataSource;
+  RidingMateAuthRepositoryImpl({
+    required RidingMateAuthRemoteDataSource remoteDataSource,
+  }) : _remoteDataSource = remoteDataSource;
 
-  final AuthRemoteDataSource _remoteDataSource;
+  final RidingMateAuthRemoteDataSource _remoteDataSource;
 
   @override
   Future<Map<String, dynamic>> loginWithGoogleIdToken({
