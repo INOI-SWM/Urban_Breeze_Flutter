a505d6b4569aff7cff856035c0f327daf9a54eeb
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-13 16:45:35 +0900

    feat: 옵션 버튼 생성

--- lib/features/my_route/presentation/screens/my_route_detail_screen.dart
+++ lib/features/my_route/presentation/screens/my_route_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ridingmate/core/extensions/theme_extensions.dart';
 import 'package:ridingmate/shared/design_system/tokens/semantic_colors.dart';
 import 'package:ridingmate/shared/layout/map_with_bottom_sheet_layout.dart';
+import 'package:ridingmate/shared/utils/platform_action_sheet.dart';
 
 class MyRouteDetailScreen extends StatelessWidget {
   const MyRouteDetailScreen({super.key});
@@ -15,7 +16,16 @@ class MyRouteDetailScreen extends StatelessWidget {
         sheetChild: const SizedBox(height: 240),
         showOptionButton: true,
         onDownloadButtonTap: (BuildContext context) {},
-        onShareButtonTap: (BuildContext context) {},
+        onShareButtonTap: (BuildContext context) {
+          showPlatformActionSheet(
+            context,
+            title: '공유 방식',
+            options: <PlatformActionSheetOption>[
+              PlatformActionSheetOption(title: '링크로 공유', onSelected: () {}),
+              PlatformActionSheetOption(title: 'GPX 파일로 공유', onSelected: () {}),
+            ],
+          );
+        },
         onOptionButtonTap: (BuildContext context) {},
       ),
     );
--- lib/features/recommended_course/presentation/screens/recommended_course_detail_screen.dart
+++ lib/features/recommended_course/presentation/screens/recommended_course_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ridingmate/core/extensions/theme_extensions.dart';
 import 'package:ridingmate/shared/design_system/tokens/semantic_colors.dart';
 import 'package:ridingmate/shared/layout/map_with_bottom_sheet_layout.dart';
+import 'package:ridingmate/shared/utils/platform_action_sheet.dart';
 
 class RecommendedCourseDetailScreen extends StatelessWidget {
   const RecommendedCourseDetailScreen({super.key});
@@ -13,8 +14,26 @@ class RecommendedCourseDetailScreen extends StatelessWidget {
       backgroundColor: colors.backgroundNormalNormal,
       body: MapWithBottomSheetLayout(
         sheetChild: const SizedBox(height: 240),
-        onDownloadButtonTap: (BuildContext context) {},
-        onShareButtonTap: (BuildContext context) {},
+        onDownloadButtonTap: (BuildContext context) {
+          showPlatformActionSheet(
+            context,
+            title: '저장 방식',
+            options: <PlatformActionSheetOption>[
+              PlatformActionSheetOption(title: '나의 경로에 저장', onSelected: () {}),
+              PlatformActionSheetOption(title: 'GPX 다운로드', onSelected: () {}),
+            ],
+          );
+        },
+        onShareButtonTap: (BuildContext context) {
+          showPlatformActionSheet(
+            context,
+            title: '공유 방식',
+            options: <PlatformActionSheetOption>[
+              PlatformActionSheetOption(title: '링크로 공유', onSelected: () {}),
+              PlatformActionSheetOption(title: 'GPX 파일로 공유', onSelected: () {}),
+            ],
+          );
+        },
       ),
     );
   }
