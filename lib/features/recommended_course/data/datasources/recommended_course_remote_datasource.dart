fe736687d1e405b680c2c96c5a6ee526b543cc6e
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-19 12:17:44 +0900

    fix: import error 수정

--- lib/features/recommended_course/data/datasources/recommended_course_remote_datasource.dart
+++ lib/features/recommended_course/data/datasources/recommended_course_remote_datasource.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
-import 'package:ridingmate/features/recommended_course/data/models/recommended_course_filter_model.dart';
 import 'package:ridingmate/features/recommended_course/data/models/recommended_course_list_data_model.dart';
+import 'package:ridingmate/features/recommended_course/data/models/recommended_course_request_model.dart';
 import 'package:ridingmate/shared/api/data/constants/api_endpoints.dart';
 import 'package:ridingmate/shared/api/data/datasources/base_remote_datasource.dart';
 import 'package:ridingmate/shared/api/data/models/api_response_model.dart';
