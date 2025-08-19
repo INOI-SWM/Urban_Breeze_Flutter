c4fd6301cdf981c1e60a5cd869a75b0948968e96
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-19 12:14:14 +0900

    feat: repository 구현체 생성

---
+++ lib/features/recommended_course/data/repositories/recommended_course_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:ridingmate/features/recommended_course/data/datasources/recommended_course_remote_datasource.dart';
+import 'package:ridingmate/features/recommended_course/data/mappers/recommended_course_request_mapper.dart';
+import 'package:ridingmate/features/recommended_course/data/mappers/recommended_course_response_mapper.dart';
+import 'package:ridingmate/features/recommended_course/data/models/recommended_course_list_data_model.dart';
+import 'package:ridingmate/features/recommended_course/data/models/recommended_course_request_model.dart';
+import 'package:ridingmate/features/recommended_course/domain/entities/recommended_course_filter.dart';
+import 'package:ridingmate/features/recommended_course/domain/entities/recommended_course_list.dart';
+import 'package:ridingmate/features/recommended_course/domain/repositories/recommended_course_repository.dart';
+import 'package:ridingmate/shared/api/data/models/api_response_model.dart';
+
+class RecommendedCourseRepositoryImpl implements RecommendedCourseRepository {
+  const RecommendedCourseRepositoryImpl({
+    required RecommendedCourseRemoteDataSource remoteDataSource,
+  }) : _remoteDataSource = remoteDataSource;
+
+  final RecommendedCourseRemoteDataSource _remoteDataSource;
+
+  @override
+  Future<RecommendedCourseList> getRecommendedCourseList(
+    RecommendedCourseFilter filter,
+  ) async {
+    final RecommendedCourseRequestModel requestModel =
+        RecommendedCourseRequestMapper.fromFilter(filter);
+
+    final ApiResponseModel<RecommendedCourseListDataModel> response =
+        await _remoteDataSource.getRecommendedCourseList(requestModel);
+
+    return RecommendedCourseResponseMapper.fromApiResponse(response);
+  }
+}
