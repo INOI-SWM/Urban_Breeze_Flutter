10d5492afb7531ece22b9caf11ee01eaec3e647b
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-25 17:29:35 +0900

    feat: terra 초기화, terra에서 연동하기 usecase생성

---
+++ lib/features/workout_history/application/use_cases/connect_terra_health_app_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:terra_flutter_bridge/models/enums.dart';
+import 'package:urban_breeze/core/result/app_result.dart';
+import 'package:urban_breeze/features/workout_history/data/datasources/terra_api_datasoiurce.dart';
+import 'package:urban_breeze/features/workout_history/domain/exceptions/workout_history_domain_exceptions.dart';
+
+class ConnectTerraHealthAppUseCase {
+  const ConnectTerraHealthAppUseCase({required this.terraDataSource});
+
+  final TerraApiDataSource terraDataSource;
+
+  Future<AppResult<void>> execute(Connection connection) async {
+    try {
+      await terraDataSource.initialiseConnection(connection);
+      return const AppSuccess<void>(null);
+    } catch (e) {
+      return AppFailure<void>(TerraApiException(e.toString()));
+    }
+  }
+}
---
+++ lib/features/workout_history/application/use_cases/initialize_terra_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:urban_breeze/core/result/app_result.dart';
+import 'package:urban_breeze/features/workout_history/data/datasources/terra_api_datasoiurce.dart';
+import 'package:urban_breeze/features/workout_history/domain/exceptions/workout_history_domain_exceptions.dart';
+
+class InitializeTerraUseCase {
+  const InitializeTerraUseCase({required this.terraDataSource});
+
+  final TerraApiDataSource terraDataSource;
+
+  Future<AppResult<void>> execute() async {
+    try {
+      await terraDataSource.initTerra();
+      return const AppSuccess<void>(null);
+    } catch (e) {
+      return AppFailure<void>(TerraApiException(e.toString()));
+    }
+  }
+}
