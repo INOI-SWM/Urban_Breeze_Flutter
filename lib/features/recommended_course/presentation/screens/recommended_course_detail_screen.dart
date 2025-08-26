ebc4c1d52033d7586753d7fa2c188d78cc2c3136
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-27 00:24:01 +0900

    feat: 추천코스 feature amplitude 도입

--- lib/features/recommended_course/presentation/screens/recommended_course_detail_screen.dart
+++ lib/features/recommended_course/presentation/screens/recommended_course_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:urban_breeze/core/amplitude/amplitude_analytics.dart';
 import 'package:urban_breeze/core/extensions/theme_extensions.dart';
 import 'package:urban_breeze/features/route_sharing/application/facades/route_sharing_facade.dart';
 import 'package:urban_breeze/features/route_sharing/di/route_sharing_providers.dart';
@@ -7,11 +8,26 @@ import 'package:urban_breeze/shared/design_system/tokens/semantic_colors.dart';
 import 'package:urban_breeze/shared/layout/map_with_bottom_sheet_layout.dart';
 import 'package:urban_breeze/shared/utils/platform_action_sheet.dart';
 
-class RecommendedCourseDetailScreen extends ConsumerWidget {
+class RecommendedCourseDetailScreen extends ConsumerStatefulWidget {
   const RecommendedCourseDetailScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RecommendedCourseDetailScreen> createState() =>
+      _RecommendedCourseDetailScreenState();
+}
+
+class _RecommendedCourseDetailScreenState
+    extends ConsumerState<RecommendedCourseDetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      AmplitudeAnalytics.logScreenView('recommended_course_detail_screen');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final SemanticColors colors = context.semanticColor;
     final RouteSharingFacade routeSharingFacade = ref.read(
       routeSharingFacadeProvider,
@@ -21,32 +37,56 @@ class RecommendedCourseDetailScreen extends ConsumerWidget {
       body: MapWithBottomSheetLayout(
         sheetChild: const SizedBox(height: 240),
         onDownloadButtonTap: (BuildContext context) {
+          AmplitudeAnalytics.logButtonClick('recommended_course_download');
+
           showPlatformActionSheet(
             context,
             title: '저장 방식',
             options: <PlatformActionSheetOption>[
-              PlatformActionSheetOption(title: '나의 경로에 저장', onSelected: () {}),
-              PlatformActionSheetOption(title: 'GPX 다운로드', onSelected: () {}),
+              PlatformActionSheetOption(
+                title: '나의 경로에 저장',
+                onSelected: () {
+                  // 나의 경로에 저장 이벤트
+                  AmplitudeAnalytics.logEvent(
+                    'recommended_course_save_to_my_route',
+                  );
+                },
+              ),
+              PlatformActionSheetOption(
+                title: 'GPX 다운로드',
+                onSelected: () {
+                  // GPX 다운로드 이벤트
+                  AmplitudeAnalytics.logEvent(
+                    'recommended_course_gpx_download',
+                  );
+                },
+              ),
             ],
           );
         },
         onShareButtonTap: (BuildContext context) {
+          AmplitudeAnalytics.logButtonClick('recommended_course_share');
+
           showPlatformActionSheet(
             context,
             title: '공유 방식',
             options: <PlatformActionSheetOption>[
               PlatformActionSheetOption(
                 title: '링크로 공유',
-                onSelected: () => routeSharingFacade.shareLink(context, 'rec1'),
+                onSelected: () {
+                  AmplitudeAnalytics.logEvent('recommended_course_share_link');
+                  routeSharingFacade.shareLink(context, 'rec1');
+                },
               ),
               PlatformActionSheetOption(
                 title: 'GPX 파일로 공유',
-                onSelected:
-                    //TODO : 추후 실제 API 요청 로직으로 변경
-                    () => routeSharingFacade.shareGpxFromAsset(
-                      context,
-                      'assets/gpx/sample.gpx',
-                    ),
+                onSelected: () {
+                  AmplitudeAnalytics.logEvent('recommended_course_share_gpx');
+                  routeSharingFacade.shareGpxFromAsset(
+                    context,
+                    'assets/gpx/sample.gpx',
+                  );
+                },
               ),
             ],
           );
