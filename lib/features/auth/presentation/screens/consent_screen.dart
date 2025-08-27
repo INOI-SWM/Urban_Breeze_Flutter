341e83489c2403b505818268f10fca932f1aab58
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-08-27 17:33:29 +0900

    feat: 동의 및 프로필 설정화면 생성

---
+++ lib/features/auth/presentation/screens/consent_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:urban_breeze/core/extensions/theme_extensions.dart';
+import 'package:urban_breeze/features/auth/di/auth_providers.dart';
+import 'package:urban_breeze/shared/design_system/tokens/typography/app_text_style.dart';
+import 'package:urban_breeze/shared/design_system/widgets/app_bar/custom_app_bar.dart';
+
+class ConsentScreen extends ConsumerWidget {
+  const ConsentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: const CustomAppBar(centerTitle: true, title: '이용약관 동의'),
+      backgroundColor: context.semanticColor.backgroundNormalNormal,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Text('이용약관 동의 화면', style: AppTextStyles.heading2.bold),
+              ),
+            ),
+            SafeArea(
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    ref
+                        .read(consentNotifierProvider.notifier)
+                        .markConsentCompleted();
+                  },
+                  child: const Text('동의 완료'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
