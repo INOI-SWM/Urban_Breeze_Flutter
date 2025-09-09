5dd93e55e06506656852d44fc2112ebfe1f3b214
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-09-09 20:58:49 +0900

    feat: 기록 갱신 datasource 생성

--- lib/features/workout_history/data/datasources/integration_authentication_datasource.dart
+++ lib/features/workout_history/data/datasources/integration_authentication_datasource.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 import 'package:urban_breeze/features/workout_history/data/models/integration_authentication_response_model.dart';
 import 'package:urban_breeze/shared/api/data/constants/api_endpoints.dart';
 import 'package:urban_breeze/shared/api/data/datasources/base_remote_datasource.dart';
+import 'package:urban_breeze/shared/api/data/models/api_response_model.dart';
 
 class IntegrationAuthenticationDataSource extends BaseRemoteDataSource {
   IntegrationAuthenticationDataSource({super.client});
@@ -22,6 +23,25 @@ class IntegrationAuthenticationDataSource extends BaseRemoteDataSource {
       return IntegrationAuthenticationApiResponse.fromJson(
         responseData,
         IntegrationAuthenticationResponseModel.fromJson,
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  /// 연동된 서비스들의 활동 기록 가져오기
+  Future<ApiResponseModel<Map<String, dynamic>>>
+  getIntegrationActivity() async {
+    try {
+      final Uri uri = Uri.parse(ApiEndpoints.integrationActivity);
+
+      final http.Response response = await get(uri.toString());
+
+      final Map<String, dynamic> responseData = decodeResponse(response);
+
+      return ApiResponseModel<Map<String, dynamic>>.fromJson(
+        responseData,
+        (Map<String, dynamic> json) => json,
       );
     } catch (e) {
       rethrow;
