0277b000907d7e6b07c2abaaeacc88baeab766f1
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-09-11 20:53:51 +0900

    feat: di provider 생성

---
+++ lib/features/profile/di/profile_providers.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:urban_breeze/core/di/core_providers.dart';
+
+import '../application/use_cases/get_profile_use_case.dart';
+import '../application/use_cases/update_birth_use_case.dart';
+import '../application/use_cases/update_gender_use_case.dart';
+import '../application/use_cases/update_introduce_use_case.dart';
+import '../application/use_cases/update_nickname_use_case.dart';
+import '../data/datasources/profile_datasource.dart';
+import '../data/repositories/profile_repository_impl.dart';
+import '../domain/repositories/profile_repository.dart';
+
+// DataSource
+final Provider<ProfileDataSource> profileDataSourceProvider =
+    Provider<ProfileDataSource>((Ref ref) {
+      final http.Client client = ref.watch(authorizedHttpClientProvider);
+      return ProfileDataSource(client: client);
+    });
+
+// Repository
+final Provider<ProfileRepository> profileRepositoryProvider =
+    Provider<ProfileRepository>((Ref ref) {
+      final ProfileDataSource dataSource = ref.watch(profileDataSourceProvider);
+      return ProfileRepositoryImpl(dataSource: dataSource);
+    });
+
+// Use Cases
+final Provider<GetProfileUseCase> getProfileUseCaseProvider =
+    Provider<GetProfileUseCase>((Ref ref) {
+      final ProfileRepository repository = ref.watch(profileRepositoryProvider);
+      return GetProfileUseCase(repository: repository);
+    });
+
+final Provider<UpdateNicknameUseCase> updateNicknameUseCaseProvider =
+    Provider<UpdateNicknameUseCase>((Ref ref) {
+      final ProfileRepository repository = ref.watch(profileRepositoryProvider);
+      return UpdateNicknameUseCase(repository: repository);
+    });
+
+final Provider<UpdateIntroduceUseCase> updateIntroduceUseCaseProvider =
+    Provider<UpdateIntroduceUseCase>((Ref ref) {
+      final ProfileRepository repository = ref.watch(profileRepositoryProvider);
+      return UpdateIntroduceUseCase(repository: repository);
+    });
+
+final Provider<UpdateBirthUseCase> updateBirthUseCaseProvider =
+    Provider<UpdateBirthUseCase>((Ref ref) {
+      final ProfileRepository repository = ref.watch(profileRepositoryProvider);
+      return UpdateBirthUseCase(repository: repository);
+    });
+
+final Provider<UpdateGenderUseCase> updateGenderUseCaseProvider =
+    Provider<UpdateGenderUseCase>((Ref ref) {
+      final ProfileRepository repository = ref.watch(profileRepositoryProvider);
+      return UpdateGenderUseCase(repository: repository);
+    });
