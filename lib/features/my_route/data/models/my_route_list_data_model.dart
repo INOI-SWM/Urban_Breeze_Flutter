b207a341a23ede2839011f1925d1533a56daddf9
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-09-15 21:13:51 +0900

    feat: my_route_list 경로범위 추가

--- lib/features/my_route/data/models/my_route_list_data_model.dart
+++ lib/features/my_route/data/models/my_route_list_data_model.dart
@@ -1,8 +1,13 @@
+import 'package:urban_breeze/features/my_route/data/models/my_route_filter_rage_model.dart';
 import 'package:urban_breeze/features/my_route/data/models/my_route_model.dart';
 import 'package:urban_breeze/features/my_route/data/models/pagination_model.dart';
 
 class MyRouteListDataModel {
-  const MyRouteListDataModel({required this.routes, required this.pagination});
+  const MyRouteListDataModel({
+    required this.routes,
+    required this.pagination,
+    required this.filterRange,
+  });
 
   factory MyRouteListDataModel.fromJson(Map<String, dynamic> json) {
     return MyRouteListDataModel(
@@ -16,11 +21,15 @@ class MyRouteListDataModel {
       pagination: PaginationModel.fromJson(
         json['pagination'] as Map<String, dynamic>,
       ),
+      filterRange: MyRouteFilterRangeModel.fromJson(
+        json['filterRange'] as Map<String, dynamic>,
+      ),
     );
   }
 
   final List<MyRouteModel> routes;
   final PaginationModel pagination;
+  final MyRouteFilterRangeModel filterRange;
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
