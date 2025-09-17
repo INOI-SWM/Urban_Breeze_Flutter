9970e8179a7de55b7780b916bcbd0eb80fda901d
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-09-17 22:00:21 +0900

    feat: minDistance, minElevation model 에 데이터추가

--- lib/features/my_route/data/models/my_route_filter_range_model.dart
+++ lib/features/my_route/data/models/my_route_filter_range_model.dart
@@ -3,21 +3,29 @@ class MyRouteFilterRangeModel {
     return MyRouteFilterRangeModel(
       maxDistance: json['maxDistance'] as double,
       maxElevationGain: json['maxElevationGain'] as double,
+      minDistance: json['minDistance'] as double,
+      minElevationGain: json['minElevationGain'] as double,
     );
   }
 
   MyRouteFilterRangeModel({
     required this.maxDistance,
     required this.maxElevationGain,
+    required this.minDistance,
+    required this.minElevationGain,
   });
 
   final double maxDistance;
+  final double minDistance;
   final double maxElevationGain;
+  final double minElevationGain;
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'maxDistance': maxDistance,
+      'minDistance': minDistance,
       'maxElevationGain': maxElevationGain,
+      'minElevationGain': minElevationGain,
     };
   }
 }
