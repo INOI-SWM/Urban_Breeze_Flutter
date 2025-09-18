a9b860946281bfa32f00207d011e957d9c75b28b
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-09-18 14:53:58 +0900

    feat: workout_list response model 및 datasource생성

--- lib/features/workout_history/data/datasources/remote_workout_history_datasource.dart
+++ lib/features/workout_history/data/datasources/remote_workout_history_datasource.dart
@@ -4,10 +4,32 @@ import 'package:urban_breeze/shared/api/data/constants/api_endpoints.dart';
 import 'package:urban_breeze/shared/api/data/datasources/base_remote_datasource.dart';
 import 'package:urban_breeze/shared/api/data/models/api_response_model.dart';
 
+import '../models/workout_list_response_model.dart';
 import '../models/workout_title_update_request_model.dart';
 
 class RemoteWorkoutHistoryDataSource extends BaseRemoteDataSource {
   RemoteWorkoutHistoryDataSource({super.client});
+
+  Future<ApiResponseModel<WorkoutListResponseModel>> getWorkoutList({
+    int page = 0,
+    int size = 10,
+  }) async {
+    final http.Response response = await get(
+      ApiEndpoints.workoutList,
+      queryParameters: <String, String>{
+        'page': page.toString(),
+        'size': size.toString(),
+      },
+    );
+
+    final Map<String, dynamic> json = decodeResponse(response);
+
+    return ApiResponseModel<WorkoutListResponseModel>.fromJson(
+      json,
+      (Map<String, dynamic> dataJson) =>
+          WorkoutListResponseModel.fromJson(dataJson),
+    );
+  }
 
   Future<ApiResponseModel<void>> updateWorkoutTitle({
     required String workoutId,
