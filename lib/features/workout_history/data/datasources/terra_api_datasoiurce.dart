6bc7bae68f55c5d7b2630e18f718556349c27549
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-09-29 20:50:03 +0900

    feat: terra sdk 인증방식 변경

--- lib/features/workout_history/data/datasources/terra_api_datasoiurce.dart
+++ lib/features/workout_history/data/datasources/terra_api_datasoiurce.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,25 +7,25 @@ import 'package:terra_flutter_bridge/models/responses.dart';
 import 'package:terra_flutter_bridge/terra_flutter_bridge.dart';
 import 'package:urban_breeze/features/auth/di/auth_providers.dart';
 import 'package:urban_breeze/features/auth/domain/entities/user.dart';
+import 'package:urban_breeze/shared/api/data/constants/api_endpoints.dart';
+import 'package:urban_breeze/shared/api/data/datasources/base_remote_datasource.dart';
 
-class TerraApiDataSource {
-  TerraApiDataSource(this.ref);
+class TerraApiDataSource extends BaseRemoteDataSource {
+  TerraApiDataSource({super.client, required this.ref});
 
   final Ref ref;
   final TerraFlutter terraFlutter = TerraFlutter();
 
   static String get _devId => dotenv.env['TERRA_DEV_ID'] ?? '';
-  static String get _apiKey => dotenv.env['TERRA_API_KEY'] ?? '';
 
   Future<void> initTerra() async {
-    final String devId = dotenv.env['TERRA_DEV_ID']!;
-
     final User? user = ref.read(userSessionNotifierProvider);
     if (user == null) {
       throw Exception('로그인된 사용자가 없습니다.');
     }
 
     final String referenceId = user.uuid;
+    final String devId = _devId;
 
     final SuccessMessage? result = await TerraFlutter.initTerra(
       devId,
@@ -56,7 +54,7 @@ class TerraApiDataSource {
     }
   }
 
-  // Terra API에서 인증 토큰 생성 TODO: 백엔드 서버에서 생성하게 변경해야함
+  // 서버를 통해 Terra API 인증 토큰 생성
   Future<String> getToken() async {
     final User? user = ref.read(userSessionNotifierProvider);
     if (user == null) {
@@ -64,21 +62,11 @@ class TerraApiDataSource {
     }
 
     try {
-      final http.Response response = await http.post(
-        Uri.parse('https://api.tryterra.co/v2/auth/generateAuthToken'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'x-api-key': _apiKey,
-          'dev-id': _devId,
-        },
-      );
+      final http.Response response = await post(ApiEndpoints.terraAuthToken);
 
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> data = jsonDecode(response.body);
-        return data['token'] as String;
-      } else {
-        throw Exception('토큰 생성 실패: ${response.statusCode} - ${response.body}');
-      }
+      final Map<String, dynamic> json = decodeResponse(response);
+      final Map<String, dynamic> data = json['data'] as Map<String, dynamic>;
+      return data['token'] as String;
     } catch (e) {
       throw Exception('토큰 생성 중 오류 발생: ${e.toString()}');
     }
