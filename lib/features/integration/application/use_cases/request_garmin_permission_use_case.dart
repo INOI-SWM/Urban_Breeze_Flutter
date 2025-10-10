59f655f223c1176952b3ddbacecd6b333a50357f
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-10-10 18:14:39 +0900

    refactor: enum으로 파라미터 교체

--- lib/features/integration/application/use_cases/request_garmin_permission_use_case.dart
+++ lib/features/integration/application/use_cases/request_garmin_permission_use_case.dart
@@ -1,6 +1,7 @@
 import 'package:urban_breeze/core/exceptions/integration_exceptions.dart';
 import 'package:urban_breeze/core/result/app_result.dart';
 import 'package:urban_breeze/features/integration/domain/entities/integration_auth.dart';
+import 'package:urban_breeze/features/integration/domain/enums/health_provider.dart';
 import 'package:urban_breeze/features/integration/domain/repositories/integration_repository.dart';
 
 class RequestGarminPermissionUseCase {
@@ -11,7 +12,7 @@ class RequestGarminPermissionUseCase {
   Future<AppResult<IntegrationAuth>> execute() async {
     try {
       final IntegrationAuth result = await repository.requestIntegrationLink(
-        terraProvider: 'GARMIN',
+        terraProvider: HealthProvider.garmin.apiProviderName,
       );
       return AppSuccess<IntegrationAuth>(result);
     } catch (e) {
--- lib/features/integration/application/use_cases/request_suunto_permission_use_case.dart
+++ lib/features/integration/application/use_cases/request_suunto_permission_use_case.dart
@@ -1,6 +1,7 @@
 import 'package:urban_breeze/core/exceptions/integration_exceptions.dart';
 import 'package:urban_breeze/core/result/app_result.dart';
 import 'package:urban_breeze/features/integration/domain/entities/integration_auth.dart';
+import 'package:urban_breeze/features/integration/domain/enums/health_provider.dart';
 import 'package:urban_breeze/features/integration/domain/repositories/integration_repository.dart';
 
 class RequestSuuntoPermissionUseCase {
@@ -11,7 +12,7 @@ class RequestSuuntoPermissionUseCase {
   Future<AppResult<IntegrationAuth>> execute() async {
     try {
       final IntegrationAuth result = await repository.requestIntegrationLink(
-        terraProvider: 'SUUNTO',
+        terraProvider: HealthProvider.suunto.apiProviderName,
       );
       return AppSuccess<IntegrationAuth>(result);
     } catch (e) {
